7b97fd4fbe3f55f1eea3009ad3daeffff48de892
Luka <bartul123@outlook.com>
2021-03-17 23:19:08 +0100

    Create FooterComponent.jsx

---
+++ src/Components/FooterComponent.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+
+// Styles
+import "./FooterComponent.css";
+
+// Back to top img
+import { backToTop } from "../assetes/index";
+
+import { inject, observer } from "mobx-react";
+
+@inject("rootStore")
+@observer
+class FooterComponent extends Component {
+  render() {
+    const {
+      showAllVehicles,
+      showingVehicles,
+    } = this.props.rootStore.mainStore.storeData;
+
+    const {
+      wrongSearchInputs,
+    } = this.props.rootStore.vehicleContainerStore.storeData;
+
+    // Change toTop image position depanding if all vehicles are shown or not
+
+    const footer = (
+      <footer className="FooterComponent">
+        <span
+          style={showAllVehicles ? { bottom: "49px" } : { bottom: "" }}
+          className="VehicleContainer-racing-flag"
+        ></span>
+        <a
+          style={showAllVehicles ? { bottom: "55px" } : { bottom: "" }}
+          className="FooterComponent-toTop"
+          href="#top"
+        >
+          <img src={backToTop} alt="to-top-page" />
+        </a>
+        <p>
+          Built by <i>Luka Bartulović</i> for <i>Mono</i>
+        </p>
+      </footer>
+    );
+    return !wrongSearchInputs && showingVehicles.length > 0 && footer;
+  }
+}
+export default FooterComponent;
