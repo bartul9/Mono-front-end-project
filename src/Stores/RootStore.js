aeadf7ba49083bbe15b36d140922fc91bb2cb527
Luka <bartul123@outlook.com>
2021-03-18 22:55:35 +0100

    Update RootStore.js

--- src/Stores/RootStore.js
+++ src/Stores/RootStore.js
@@ -1,22 +1,24 @@
 import VehicleContainerStore from "./VehiclesStores/VehicleContainerStore";
 import MainStore from "./VehiclesStores/MainStore";
 import VehicleCardStore from "./VehiclesStores/VehicleCardStore";
-import CreateNewVehicleStore from "./VehiclesStores/CreateNewVehicleStore";
+import CreateVehicleStore from "./VehiclesStores/CreateVehicleStore";
 import MakeStore from "./MakeStores/MakeStore";
 import CreateMakeCardStore from "./MakeStores/CreateMakeCardStore";
 import SharedFunctionsStore from "./SharedFunctionsStore/SharedFunctionsStore";
 import DeleteMakeCardStore from "./MakeStores/DeleteMakeCardStore";
+import WarningMessageStore from "./CommonStores/WarningMessageStore";
 
 class RootStore {
   constructor() {
+    this.deleteMakeCardStore = new DeleteMakeCardStore(this);
+    this.sharedFunctionsStore = new SharedFunctionsStore(this);
+    this.createMakeCardStore = new CreateMakeCardStore(this);
+    this.makeStore = new MakeStore(this);
+    this.createVehicleStore = new CreateVehicleStore(this);
+    this.vehicleCardStore = new VehicleCardStore(this);
+    this.warningMessageStore = new WarningMessageStore(this);
     this.mainStore = new MainStore(this);
     this.vehicleContainerStore = new VehicleContainerStore(this);
-    this.vehicleCardStore = new VehicleCardStore(this);
-    this.createNewVehicleStore = new CreateNewVehicleStore(this);
-    this.makeStore = new MakeStore(this);
-    this.createMakeCardStore = new CreateMakeCardStore(this);
-    this.sharedFunctionsStore = new SharedFunctionsStore(this);
-    this.deleteMakeCardStore = new DeleteMakeCardStore(this);
   }
 }
 
