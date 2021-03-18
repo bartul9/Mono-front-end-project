43ce19678f648e539dccb32cea1fd7ae7a215b3d
Luka <bartul123@outlook.com>
2021-03-18 22:56:15 +0100

    Update HomePage.jsx

--- src/Pages/Vehicles/Home/HomePage.jsx
+++ src/Pages/Vehicles/Home/HomePage.jsx
@@ -3,18 +3,19 @@ import React from "react";
 // Styles
 import "./HomePage.css";
 
+// Header background image
 import { home_background } from "../../../images/index";
 
 // Components
-import VehicleContainer from "../Components/VehicleContainer";
-import FooterComponent from "../../../Components/FooterComponent";
 import HeaderComponent from "../../../Components/HeaderComponent";
 import WarningMessage from "../../../Components/WarningMessage";
+import VehicleContainer from "../Components/VehicleContainer";
+import FooterComponent from "../../../Components/FooterComponent";
 
 function Home() {
   return (
     <section className="HomePage">
-      <WarningMessage backgroundClr={"backgroundBlue"} />
+      <WarningMessage />
       <HeaderComponent text={"Garage"} background={home_background} />
       <VehicleContainer />
       <FooterComponent />
