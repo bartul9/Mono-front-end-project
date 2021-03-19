45063d9604c50aa1133ded76d919da66ddc75bda
Luka Bartulović <74148005+bartul9@users.noreply.github.com>
2021-03-19 18:39:04 +0100

    Update index.js

--- src/assetes/index.js
+++ src/assetes/index.js
@@ -1,7 +1,6 @@
 export { default as showmore } from "./show-more.png";
 export { default as logo } from "./garage-logo.png";
 export { default as navbarlogo } from "./garage-transparent.png";
-export { default as aston_transparent } from "./aston-transparent.png";
 export { default as backToTop } from "./back-to-top.png";
 export { default as create_icon } from "./create.png";
 export { default as factory_icon } from "./car-factory.jpg";
