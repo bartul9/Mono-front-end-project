56814998dcd9ef45cab5d84cc51a645957325659
Luka <bartul123@outlook.com>
2021-03-19 23:49:49 +0100

    Create index.js

---
+++ src/assetes/index.js
@@ -0,0 +1,4 @@
+export { default as showmore } from "./show-more.png";
+export { default as navbarlogo } from "./garage-transparent.png";
+export { default as backToTop } from "./back-to-top.png";
+export { default as makeToTop } from "./make-to-top.png";
