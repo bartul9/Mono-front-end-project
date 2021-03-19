53908ee58b7a05d4b75a472c19daff3c87131360
Luka Bartulović <74148005+bartul9@users.noreply.github.com>
2021-03-19 23:39:32 +0100

    Delete src directory

--- src/assetes/index.js
+++
@@ -1,7 +0,0 @@
-export { default as showmore } from "./show-more.png";
-export { default as logo } from "./garage-logo.png";
-export { default as navbarlogo } from "./garage-transparent.png";
-export { default as backToTop } from "./back-to-top.png";
-export { default as create_icon } from "./create.png";
-export { default as factory_icon } from "./car-factory.jpg";
-export { default as makeToTop } from "./make-to-top.png";
--- src/images/index.js
+++
@@ -1,2 +0,0 @@
-export { default as home_background } from "./header-background.jpg";
-export { default as editing_garage } from "./editing-garage.jpg";
