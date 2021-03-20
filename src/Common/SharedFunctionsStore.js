00b9f150ba8fa73ad5f8095a14ed448bee7c4baa
Luka <bartul123@outlook.com>
2021-03-20 13:22:48 +0100

    Update SharedFunctionsStore.js

--- src/Common/SharedFunctionsStore.js
+++ src/Common/SharedFunctionsStore.js
@@ -7,19 +7,14 @@ class SharedFunctionsStore {
   }
 
   // Function for checking if all inputs are filled
-  checkValidInputs = (inputs) => {
-    const arr = Object.values(inputs);
-    return arr.every((value) => value.length > 0);
-  };
+  checkValidInputs = (inputs) =>
+    Object.values(inputs).every((value) => value.length > 0);
 
   // Function for toggling if toTop icon is showing or not in MakePage, but since it can be used elsewhere I decided to put it here
-  handleScroll = () => {
-    if (window.pageYOffset >= 20) {
-      this.storeData.scrolling = true;
-    } else {
-      this.storeData.scrolling = false;
-    }
-  };
+  handleScroll = () =>
+    window.pageYOffset >= 20
+      ? (this.storeData.scrolling = true)
+      : (this.storeData.scrolling = false);
 }
 
 export default SharedFunctionsStore;
