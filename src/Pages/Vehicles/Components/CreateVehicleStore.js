c263d168d9b1d5307f3ef2199c03cb8e6ede60a6
Luka <bartul123@outlook.com>
2021-03-20 12:39:44 +0100

    Update CreateVehicleStore.js

--- src/Pages/Vehicles/Components/CreateVehicleStore.js
+++ src/Pages/Vehicles/Components/CreateVehicleStore.js
@@ -18,7 +18,7 @@ class CreateNewVehicleStore {
     });
   }
 
-  resetInput = () => {
+  resetInputs = () => {
     this.storeData.newVehicle = {
       make: "",
       model: "",
@@ -38,23 +38,26 @@ class CreateNewVehicleStore {
     this.updateProperty(event.target.name, event.target.value);
   };
 
-  // If all inputs are filled update vehicles array in the store with new created vehicle. Clean local state, so all inputs are empty, also call displayCreateNewVehicle function which closes the modal window, if there was error display warningMessage
+  // If all inputs are filled update vehicles array with new vehicle. Clean local state, so all inputs are empty, also call displayCreateNewVehicle function which closes the modal window, if there was error display warningMessage
   handleSubmit = (e) => {
     e.preventDefault();
+
     if (
       this.rootStore.sharedFunctionsStore.checkValidInputs(
         this.storeData.newVehicle
       )
     ) {
-      const vehicle = { ...this.storeData.newVehicle, id: uuid() };
       this.rootStore.vehicleContainerStore.storeData.showAllVehicles = false;
       this.rootStore.vehicleContainerStore.storeData.currentPage = 1;
+
+      const vehicle = { ...this.storeData.newVehicle, id: uuid() };
+      this.rootStore.vehicleService.addVehicle(vehicle);
       this.rootStore.vehicleContainerStore.storeData.showingVehicles = [
         vehicle,
       ];
-      this.rootStore.vehicleService.addVehicle(vehicle);
+
       this.displayCreateNewVehicle();
-      this.resetInput();
+      this.resetInputs();
     } else {
       this.rootStore.warningMessageStore.setWarningMessage(
         true,
@@ -64,10 +67,11 @@ class CreateNewVehicleStore {
     }
   };
 
-  // Function for toggling modal display, also perform check if the warningMessage was showing, if true, remove it
+  // Function for toggling modal display, also perform check if warningMessage was showing, if true, remove it
   displayCreateNewVehicle = () => {
     this.storeData.displayingCreateNewVehicle = !this.storeData
       .displayingCreateNewVehicle;
+
     if (this.storeData.displayingCreateNewVehicle === false) {
       this.rootStore.warningMessageStore.setWarningMessage(false, "", "");
     }
