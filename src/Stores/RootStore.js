6365a5b760fc7450d08479a68c35179cbdf4eb7d
Luka <bartul123@outlook.com>
2021-03-20 12:38:22 +0100

    Update RootStore.js

--- src/Stores/RootStore.js
+++ src/Stores/RootStore.js
@@ -1,26 +1,30 @@
+import VehicleService from "../Services/VehicleService";
 import VehicleContainerStore from "../Pages/Vehicles/Components/VehicleContainerStore";
 import VehicleCardStore from "../Pages/Vehicles/Components/VehicleCardStore";
 import CreateVehicleStore from "../Pages/Vehicles/Components/CreateVehicleStore";
+
 import MakeService from "../Services/MakeService";
-import CreateMakeStore from "../Pages/Make/Components/CreateMakeStore";
-import SharedFunctionsStore from "../Common/SharedFunctionsStore";
-import DeleteMakeStore from "../Pages/Make/Components/DeleteMakeStore";
-import WarningMessageStore from "../Components/WarningMessageStore";
-import VehicleService from "../Services/VehicleService";
 import MakeStore from "../Pages/Make/MakeStore";
+import CreateMakeStore from "../Pages/Make/Components/CreateMakeStore";
+import DeleteMakeStore from "../Pages/Make/Components/DeleteMakeStore";
+
+import WarningMessageStore from "../Components/WarningMessageStore";
+import SharedFunctionsStore from "../Common/SharedFunctionsStore";
 
 class RootStore {
   constructor() {
-    this.createVehicleStore = new CreateVehicleStore(this);
-    this.vehicleCardStore = new VehicleCardStore(this);
-    this.warningMessageStore = new WarningMessageStore(this);
-    this.deleteMakeStore = new DeleteMakeStore(this);
-    this.sharedFunctionsStore = new SharedFunctionsStore(this);
     this.vehicleService = new VehicleService(this);
-    this.createMakeStore = new CreateMakeStore(this);
+    this.vehicleContainerStore = new VehicleContainerStore(this);
+    this.vehicleCardStore = new VehicleCardStore(this);
+    this.createVehicleStore = new CreateVehicleStore(this);
+
     this.makeService = new MakeService(this);
     this.makeStore = new MakeStore(this);
-    this.vehicleContainerStore = new VehicleContainerStore(this);
+    this.createMakeStore = new CreateMakeStore(this);
+    this.deleteMakeStore = new DeleteMakeStore(this);
+
+    this.warningMessageStore = new WarningMessageStore(this);
+    this.sharedFunctionsStore = new SharedFunctionsStore(this);
   }
 }
 
