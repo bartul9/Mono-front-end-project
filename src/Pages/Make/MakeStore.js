282aed5a8dbc0422a1d72f8752cffed1facc82ad
Luka <bartul123@outlook.com>
2021-03-24 12:27:46 +0100

    Update MakeStore.js

--- src/Pages/Make/MakeStore.js
+++ src/Pages/Make/MakeStore.js
@@ -10,6 +10,9 @@ class MakeStore {
 
   // Set makePage to be true and show diffrent buttons, clean up the warning message and hide it if it was showing in Home or Edit page, add scroll event for showing to top icon on scroll
   componentDidMountSetData = () => {
+    this.rootStore.vehicleContainerStore.storeData.showAllVehicles = false;
+    this.rootStore.paginationStore.storeData.currentPage = 1;
+    this.rootStore.paginationStore.storeData.postsPerPage = 8;
     this.rootStore.vehicleContainerStore.storeData.makePage = true;
     this.rootStore.warningMessageStore.setWarningMessage(false, "", "");
 
@@ -21,18 +24,14 @@ class MakeStore {
 
   // Change all the states on unmount so everything is cleaned and working when user switches back to Edit or Home page
   componentWillUnmountCleanUp = () => {
+    this.rootStore.paginationStore.storeData.currentPage = 1;
+    this.rootStore.paginationStore.storeData.postsPerPage = 8;
     this.rootStore.vehicleContainerStore.storeData.makePage = false;
     this.rootStore.vehicleContainerStore.storeData.showingVehicles = this.rootStore.vehicleService.getVehicles();
     this.rootStore.vehicleContainerStore.storeData.searchName = "";
 
     this.rootStore.deleteMakeStore.storeData.deleting = false;
     this.rootStore.warningMessageStore.setWarningMessage(false, "", "");
-
-    if (this.rootStore.vehicleService.getVehicles().length < 9) {
-      this.rootStore.vehicleContainerStore.storeData.showAllVehicles = true;
-    } else {
-      this.rootStore.vehicleContainerStore.storeData.showAllVehicles = false;
-    }
 
     window.removeEventListener(
       "scroll",
