00380bfbf4525e5fac638c616bb342cdf4367b63
Luka <bartul123@outlook.com>
2021-03-24 12:27:34 +0100

    Update SortingFilteringComponent.jsx

--- src/Pages/Vehicles/Components/SortingFilteringComponent.jsx
+++ src/Pages/Vehicles/Components/SortingFilteringComponent.jsx
@@ -32,73 +32,86 @@ class SortingFilteringComponent extends Component {
 
     const { getMakes } = this.props.rootStore.makeService;
 
+    const {
+      editingCard,
+    } = this.props.rootStore.vehicleCardStore.storeData.editingInputs;
+
     return (
       <div
         className={`VehicleContainer-sorting-filtering ${
           moreOptions && "VehicleContainer-sorting-filtering-click"
         }`}
       >
-        <span
-          style={moreOptions ? { left: "-150%" } : { left: "18px" }}
-          id="VehicleContainer-more"
-          onClick={handleShowingMore}
-        >
-          <img style={{ width: "30px" }} src={showmore} alt="show-more-img" />
-        </span>
+        {!editingCard && (
+          <span
+            style={moreOptions ? { left: "-150%" } : { left: "18px" }}
+            id="VehicleContainer-more"
+            onClick={handleShowingMore}
+          >
+            <img style={{ width: "30px" }} src={showmore} alt="show-more-img" />
+          </span>
+        )}
         <span id="topContainer"></span>
-        <div
-          style={moreOptions ? { right: "0%" } : { right: "-100%" }}
-          className="VehicleContainer-options"
-        >
-          <div className="VehicleContainer-filtering">
-            <label htmlFor="cars">Make: </label>
-            <select
-              className="select-css"
-              onChange={(e) => filterByMake(e.target.value)}
-              value={optionValue}
-            >
-              <option></option>
-              {getMakes().map((make) => {
-                return (
-                  <option key={make.make} value={make.make}>
-                    {make.make}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
-          <span onClick={showAll}>
-            Show{" "}
-            {showAllVehicles
-              ? showingVehicles.length < 9
-                ? "All"
-                : "Less"
-              : "All"}
-          </span>
-          <span onClick={sortByYear}>
-            Sort by Year{" "}
-            {sortingByYear ? (
-              <b className="VehicleContainer-arrow">&#8593;</b>
-            ) : (
-              <b className="VehicleContainer-arrow">&#8595;</b>
-            )}
-          </span>
-          <span onClick={sortByHorsepower}>
-            Sort by Horsepower{" "}
-            {sortingByHorsepower ? (
-              <b className="VehicleContainer-arrow">&#8593;</b>
-            ) : (
-              <b className="VehicleContainer-arrow">&#8595;</b>
-            )}
-          </span>
-          {moreOptions && (
-            <span
-              onClick={handleShowingLess}
-              className="close-sorting-filtering"
-            >
-              Close
+        {editingCard && (
+          <span style={{ marginRight: "45px" }}>Editing Vehicle</span>
+        )}
+        <div style={editingCard ? { display: "none" } : { display: "block" }}>
+          <div
+            style={moreOptions ? { right: "0%" } : { right: "-100%" }}
+            className="VehicleContainer-options"
+          >
+            <div className="VehicleContainer-filtering">
+              <label htmlFor="cars">Make: </label>
+              <select
+                className="select-css"
+                onChange={(e) => filterByMake(e.target.value)}
+                value={optionValue}
+              >
+                <option></option>
+                {getMakes().map((make) => {
+                  return (
+                    <option key={make.make} value={make.make}>
+                      {make.make}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+
+            <span onClick={showAll}>
+              Show{" "}
+              {showAllVehicles
+                ? showingVehicles.length < 9
+                  ? "All"
+                  : "Less"
+                : "All"}
             </span>
-          )}
+
+            <span onClick={sortByYear}>
+              Sort by Year{" "}
+              {sortingByYear ? (
+                <b className="VehicleContainer-arrow">&#8593;</b>
+              ) : (
+                <b className="VehicleContainer-arrow">&#8595;</b>
+              )}
+            </span>
+            <span onClick={sortByHorsepower}>
+              Sort by Horsepower{" "}
+              {sortingByHorsepower ? (
+                <b className="VehicleContainer-arrow">&#8593;</b>
+              ) : (
+                <b className="VehicleContainer-arrow">&#8595;</b>
+              )}
+            </span>
+            {moreOptions && (
+              <span
+                onClick={handleShowingLess}
+                className="close-sorting-filtering"
+              >
+                Close
+              </span>
+            )}
+          </div>
         </div>
       </div>
     );
