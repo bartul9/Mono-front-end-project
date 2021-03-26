57c1bb33bae40b45daf4fa6625ac08c2c2cb184a
Luka <bartul123@outlook.com>
2021-03-26 11:16:20 +0100

    Update MakeService.js

--- src/Common/Vehicle-Make-Services/MakeService.js
+++ src/Common/Vehicle-Make-Services/MakeService.js
@@ -12,8 +12,6 @@ import {
   ford_logo,
 } from "../../images/MakeImages";
 
-import { v4 as uuid } from "uuid";
-
 class MakeService {
   makes = [
     {
@@ -21,70 +19,70 @@ class MakeService {
       country: "Germany",
       founded: "March 7, 1916",
       logo: bmw_logo,
-      id: uuid(),
+      id: 1,
     },
     {
       make: "Mercedes",
       country: "Germany",
       founded: "1926",
       logo: mercedes_logo,
-      id: uuid(),
+      id: 2,
     },
     {
       make: "Audi",
       country: "Germany",
       founded: "June 29, 1932",
       logo: audi_logo,
-      id: uuid(),
+      id: 3,
     },
     {
       make: "Ferrari",
       country: "Italy",
       founded: "1947",
       logo: ferrari_logo,
-      id: uuid(),
+      id: 4,
     },
     {
       make: "Lamborghini",
       country: "Italy",
       founded: "May 1963",
       logo: lamborghini_logo,
-      id: uuid(),
+      id: 5,
     },
     {
       make: "Porsche",
       country: "Germany",
       founded: "April 25, 1931",
       logo: porsche_logo,
-      id: uuid(),
+      id: 6,
     },
     {
       make: "Nissan",
       country: "Japan",
       founded: "December 26, 1933",
       logo: nissan_logo,
-      id: uuid(),
+      id: 7,
     },
     {
       make: "Mitsubishi",
       country: "Japan",
       founded: "1870",
       logo: mitsubishi_logo,
-      id: uuid(),
+      id: 8,
     },
     {
       make: "Aston Martin",
       country: "Great Britain",
       founded: "January 15, 1913",
       logo: astonMartin_logo,
-      id: uuid(),
+      id: 9,
     },
     {
       make: "Ford",
       country: "USA",
       founded: "June 16, 1903",
       logo: ford_logo,
-      id: uuid(),
+      id: 10,
     },
   ];
 
